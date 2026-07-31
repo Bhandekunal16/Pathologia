1980ccc1241357753dd56c5657e71c7b793a2cfd
bhandekunal16 <bhandekunal16@gmail.com>
2026-07-31 12:24:34 +0530

    Refactor AuditModule: Simplify MongooseModule import by consolidating schema definition into a single line for improved readability and maintainability.

--- code/pathologia-server/src/audit/audit.module.ts
+++ code/pathologia-server/src/audit/audit.module.ts
@@ -4,12 +4,10 @@ import { AuditController } from './controllers/audit.controller';
 import { AuditLog, AuditLogSchema } from './schemas/audit-log.schema';
 import { AuditService } from './services/audit.service';
 
+const { name } = AuditLog;
+
 @Module({
-  imports: [
-    MongooseModule.forFeature([
-      { name: AuditLog.name, schema: AuditLogSchema },
-    ]),
-  ],
+  imports: [MongooseModule.forFeature([{ name, schema: AuditLogSchema }])],
   controllers: [AuditController],
   providers: [AuditService],
   exports: [AuditService],
