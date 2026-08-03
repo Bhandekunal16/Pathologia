d00a4c687128602000f31519c1ef7e3360f1b672
bhandekunal16 <bhandekunal16@gmail.com>
2026-08-03 12:59:38 +0530

    Refactor Accept Invite DTO: Simplify import statements by consolidating class-validator imports, enhancing code readability and maintainability.

--- code/pathologia-server/src/invites/dto/accept-invite.dto.ts
+++ code/pathologia-server/src/invites/dto/accept-invite.dto.ts
@@ -1,10 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import {
-  IsNotEmpty,
-  IsString,
-  MaxLength,
-  MinLength,
-} from 'class-validator';
+import { IsNotEmpty, IsString, MaxLength, MinLength } from 'class-validator';
 import { IsStrongPassword } from '../../common/validators/is-strong-password.decorator';
 
 export class AcceptInviteDto {
